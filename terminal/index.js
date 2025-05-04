3f6a8e1fd098932380cad6e10fe018cb47c5bda4
CSof-student <122061715+CSof-student@users.noreply.github.com>
2025-05-04 11:13:55 -0700

    websitea can take in and process input :wq

---
+++ terminal/index.js
@@ -0,0 +1,22 @@
+
+import { runCommand } from "./commandRouter.js"
+import {shellContext} from "./shell.js"
+import {parseInput} from "./parser.js";
+
+let command;
+
+// document.getElementById("placeholder button").onclick = function(){
+//     console.log("button clicked");
+//     command = document.getElementById("my text").value;
+//     console.log(runCommand(parseInput(command),shellContext));
+
+    
+
+// }
+const input= document.getElementById("command-input");
+
+    input.addEventListener('keydown', (e)=>{
+        if(e.key == 'Enter') {
+            console.log(runCommand(parseInput(input.value),shellContext))
+        }
+    } )
